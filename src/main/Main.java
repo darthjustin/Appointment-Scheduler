d38387dab3a52cd569b076e4c9f53d024ccf161a
darthjustin <lovett.justin90@gmail.com>
2023-05-13 15:58:33 -0700

    Changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,9 +7,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
 public class Main extends Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
         JDBC.openConnection();
 
@@ -20,8 +29,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        ResourceBundle rb = ResourceBundle.getBundle("language/Nat", Locale.getDefault());
         Parent root = FXMLLoader.load(getClass().getResource("/view/login-view.fxml"));
-        stage.setTitle("User Login");
+        stage.setTitle(rb.getString("title"));
         stage.setScene(new Scene(root));
         stage.show();
     }
